96846b2da93d90db9ef6edaa91ce0c9728cdc5bf
Nehal <nehaaal92@gmail.com>
2022-07-12 23:16:34 +0200

    1.0 v of weather app

--- projects/weather-journal-app/server.js
+++ projects/weather-journal-app/server.js
@@ -1,9 +1,13 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors())
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = '8083';
+function listener(){
+    console.log(`Example app running on http://localhost:${port}`);
+}
+const server = app.listen(port, listener);
+
+app.post('/newEntry', (req, res) => {
+    const newEntry = req.body;
+    projectData.push(newEntry);
+    console.log('New entry to projectData', projectData);
+})
+
+app.get('/updatedData', (req, res) => {
+    res.send(projectData.pop());
+    console.log('Data sent to a client side');
+})
--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,5 +1,139 @@
 /* Global Variables */
+const apiKey = '21266dcdc592c6cd18f5e49a2317e9f4&units=imperial';
 
+const icons = document.getElementById('weather__icon').children
+const app = document.getElementById('app')
+const generateBtn = document.getElementById('generate')
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+// Get the data from the server 
+const getData = async (url='') => {
+    // fetch data
+    const res = await fetch(url)
+
+    // check recieved response
+    try{
+        const resData = await res.json();
+        return(resData)
+    } catch(error){
+        console.log(error)
+    }
+}
+
+// Post data to server 
+const postData = async (url='', data={}) => {
+    // send data to server
+    const res = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    try{
+        return res
+    } catch(error){
+        console.log(error)
+    }
+}
+/* update UI*/
+async function updateUI(){
+    getData('/updatedData').then(data=>{
+        // update text content
+        document.getElementById('temp').textContent = Math.round((Math.round(data.temp)-32) *5 / 9)+ '°C';
+        document.getElementById('content').textContent = data.userFeelings;
+        document.getElementById('date').textContent = data.date;
+        let description = '';
+        data.description.forEach(i=>{
+            description += i + ' ,';
+        })
+        document.getElementById('description').textContent = description.slice(0, -1);
+        // update weather icon
+        data.condition.forEach(i=>{
+            switch(i){
+                case 'Thunderstorm':
+                    icons[3].classList.remove('hidden')
+                    icons[3].classList.add('thunder', 'lightening')
+                    icons[4].classList.remove('hidden')
+                    icons[4].classList.add('thunder')
+                    icons[5].classList.replace('hidden', 'thunder_icon')
+                    break;
+                case 'Drizzle':
+                    icons[3].classList.remove('hidden')
+                    icons[4].classList.remove('hidden')
+                    icons[0].classList.remove('hidden')
+                    icons[3].classList.add('thunder')
+                    icons[4].classList.add('thunder')
+                    break
+                case 'Rain':
+                    icons[3].classList.remove('hidden')
+                    icons[4].classList.remove('hidden')
+                    icons[0].classList.remove('hidden')
+                    break
+                case 'Snow':
+                    icons[3].classList.remove('hidden')
+                    icons[4].classList.remove('hidden')
+                    icons[1].classList.remove('hidden')
+                    break
+                case 'Clear':
+                    icons[2].classList.remove('hidden')
+                    break
+                case 'Clouds':
+                    icons[3].classList.remove('hidden')
+                    icons[4].classList.remove('hidden')
+                    break
+                default:
+                    icons[3].classList.remove('hidden')
+                    icons[4].classList.remove('hidden')
+                    icons[6].classList.remove('hidden')
+                    icons[3].classList.add('thunder')
+                    icons[4].classList.add('thunder')
+            }
+            
+
+        })
+
+
+        
+    })
+}
+/* callbacks */
+function generateData(){
+    const zip = document.getElementById('zip').value;
+    const userFeelings = document.getElementById('feelings').value
+    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=imperial`
+    // change bg state
+    app.classList.replace('darky', 'blur')
+    // get the data from the api then store it to the server then update the UI
+    getData(url).then((weatherData) => {
+        if(weatherData.cod !== 200){
+            console.log('404 Error')
+            return 
+        }
+        const description=[];
+        const condition=[];
+        weatherData.weather.forEach(i => {
+            description.push(i.description);
+            condition.push(i.main);
+        })
+
+        const dataObj = {
+            temp: weatherData.main.temp,
+            date: newDate,
+            userFeelings,
+            description,
+            condition
+
+        }
+        postData('/newEntry', dataObj)
+    }).then(updateUI)
+
+
+}
+
+/* Events */
+generateBtn.addEventListener('click', generateData)
+
